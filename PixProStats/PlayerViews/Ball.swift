490f19e084b428bf3bbf18f64e94dee5b04b053d
Isaiah <isaiahhernandez812@gmail.com>
2025-03-23 15:32:24 -0500

    Added pitch types to player info view

---
+++ PixProStats/PlayerViews/Ball.swift
@@ -0,0 +1,18 @@
+//
+//  Ball.swift
+//  PixProStats
+//
+//  Created by Isaiah Hernandez on 3/13/25.
+//
+
+import SwiftUI
+
+struct Ball: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ball()
+}
---
+++ PixProStats/PlayerViews/PitchView.swift
@@ -0,0 +1,18 @@
+//
+//  PitchView.swift
+//  PixProStats
+//
+//  Created by Isaiah Hernandez on 3/13/25.
+//
+
+import SwiftUI
+
+struct PitchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PitchView()
+}
